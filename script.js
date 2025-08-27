bac9c189bb368263b8fd74b756d1a6ad9707bd86
01-akm <kraadi4941@gmail.com>
2025-08-27 11:24:08 +0530

    Implement Calculator class to encapsulate core logic

---
+++ script.js
@@ -0,0 +1,7 @@
+ // A class to handle all calculator logic
+        class Calculator {
+            constructor(previousOperandTextElement, currentOperandTextElement) {
+                this.previousOperandTextElement = previousOperandTextElement;
+                this.currentOperandTextElement = currentOperandTextElement;
+                this.clear();
+            }
